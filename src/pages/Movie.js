7fd9ba64e1d5602e5eb30663ab57ba6cdb4c615a
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-06-04 23:47:09 +0300

    feat: Add routing functionality

--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movie, setMovie] = useState({title: '', time: 0, genres: [] })
+  const params = useParams()
+  const movieId = params.id
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${movieId}`)
+    .then(response => response.json())
+    .then(data => setMovie(data))
+  }, [movieId])
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movie.title}</h1>
+        <p>{movie.time} minutes</p>
+        {movie.genres.map((genre, index) => <span key={index}>{genre}</span>)}
       </main>
     </>
   );
